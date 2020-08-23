0c1cead36b54f05ab99f61b9b9264d65072c16b2
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-08-22 21:04:37 -0600

    Questions are readble from array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,25 @@
 var timerEl = document.getElementById("time-left");
+var beginQuiz = document.getElementById("quiz-beg");
+var quesStart = document.getElementById("question-start");
+var btnStart = document.getElementById("begin-btn");
+var buttonA = document.getElementById("btn-a");
+var buttonB = document.getElementById("btn-b");
+var buttonC = document.getElementById("btn-c");
+var buttonD = document.getElementById("btn-d");
+var questionHead = document.getElementById("question-header");
 var timeLeft = 10;
+var questionCount = 0;
+var questionsArr = [
+    { q: "What color is the sky?", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Quesiton2", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question3", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question4", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question5", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question6", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question7", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question8", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question9", a: "blue", b: "green", c: "black", d: "red"},
+    { q: "Question10", a: "blue", b: "green", c: "black", d: "red"}];
 
 var countDown = function(){
     timerEl.textContent = "Time: " + timeLeft;
@@ -9,12 +29,38 @@ var countDown = function(){
         timerEl.textContent = "Time: " + timeLeft;
     }
     if(timeLeft === 0){
-        console.log("This worked");
         clearInterval(timeInterval);
     }
-
     }, 1000);
 }
 
+var startQuiz = function(){
+    beginQuiz.style = "display: none";
+    quizLoop();
+    countDown();
+}
 
-countDown();
+var quizLoop = function(){
+    quesStart.style = "display: block";
+    questionHead.textContent = questionsArr[questionCount].q;
+    buttonA.textContent = questionsArr[questionCount].a;
+    buttonB.textContent = questionsArr[questionCount].b;
+    buttonC.textContent = questionsArr[questionCount].c;
+    buttonD.textContent = questionsArr[questionCount].d;
+}
+var nextQuestion = function(){
+    if(questionCount === questionsArr.length - 1){
+        endQuiz();
+        return;
+    }
+    questionCount++;
+    quizLoop();
+}
+
+
+btnStart.onclick = startQuiz;
+buttonA.addEventListener("click",nextQuestion);
+buttonB.addEventListener("click",nextQuestion);
+buttonC.addEventListener("click",nextQuestion);
+buttonD.addEventListener("click",nextQuestion);
+console.log(questionsArr.length);
